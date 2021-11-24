84fd5b9b5c2f69207f014e337b0ac2ebfb7f43f2
unknown <hamzahouri@gmail.com>
2021-11-24 23:04:00 +0100

    last change in the project adding the delete method

--- script.js
+++ script.js
@@ -15,16 +15,37 @@ function showModal () {
 modalShow.addEventListener('click',showModal);
 modalClose.addEventListener('click', ()=>{modal.classList.remove('show-modal')});
 window.addEventListener('click',(e)=>{e.target===modal ? modal.classList.remove('show-modal') : false});
-
-// validate form
-/*
-function validate (nameValue,urlValue) {
-    if(nameValue==" " && urlValue==" ") {
-        alert("please fill all fields");
-        return false;
+ 
+function buildBookmarks () {
+    //remove all bookmarks element
+    bookmarksContainer.textContent= '';
+    bookmarks.forEach((bookmark)=> {
+        const {name, url} = bookmark;
+        // item
+        const item = document.createElement('div');
+        item.classList.add('item');
+        // close item
+        const closeIcon = document.createElement('i');
+        closeIcon.classList.add('fas','fa-times');
+        closeIcon.setAttribute('title', 'delete bookmark');
+        closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+        //Favicon / link container
+        const linkInfo = document.createElement('div');
+        linkInfo.classList.add('name');
+        // favicon
+        const favicon=document.createElement('img');
+        // link
+        const link = document.createElement('a');
+        link.setAttribute('href', `${url}`);
+        link.setAttribute('target', '_blank');
+        link.textContent = name;
+        linkInfo.append(favicon , link);
+        item.append(closeIcon,linkInfo);
+        bookmarksContainer.appendChild(item);
+        console.log(bookmarksContainer);
+    });
 }
 
-}*/
 //Function that fetch data from local storage, if availaible
 function fetchBookmarks() {
     //get bookmarks from local if availaible
@@ -38,9 +59,22 @@ function fetchBookmarks() {
                 url : "https://google.com"
             },
         ];
-        localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+        localStorage.setItem('bookmarks',JSON.stringify(bookmarks));  
     }
-    console.log(bookmarks)
+    buildBookmarks();
+    console.log(bookmarks);
+}
+
+// Delete bookmark
+function deleteBookmark (url) {
+    bookmarks.forEach((book,i)=> {
+     if(book.url===url) {
+         bookmarks.splice(i,1);
+     }
+    });
+    // update bookmarks array in local storage and re-populate DOM
+    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    fetchBookmarks(); 
 }
 
 // handle Data from form 
@@ -65,10 +99,13 @@ function storeBookmark (e) {
         fetchBookmarks();
         bookmarkForm.reset();
         websiteNameEl.focus();
+        
     }
    
    
 }
+
+
 
 // event listner 
 bookmarkForm.addEventListener('submit', storeBookmark);
